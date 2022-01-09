c5ed1365f5c29de8d0cb9fcec7dca50fe82c8271
majesuso <majesus.op@gmail.com>
2022-01-09 20:13:55 -0300

    mi primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+
+let enviar = document.getElementById("enviar"), // Contiene todos los input, id de form
+    cardNumber = document.getElementById("cardNumber"); // Input de cardNumber
+//arrayUnmasked = []; // Array vacío que irá guardando los números de cardNumber
+
+enviar.addEventListener("submit", function isValid(e) { // escucha eventos en todos los input 
+    e.preventDefault();
+
+    let name = document.getElementById("user").value, // nombre usuario
+        card = cardNumber.value, //arrayUnmasked.join(""), // número de tarjeta 
+        overlay = document.getElementById("overlay"), // modal
+        validationResult = validator.isValid(card), // trae la función isValid
+        maskNumber = validator.maskify(card); // trae la función maskify
+
+    // condicionales para mensajes de válida o inválida
+    if (name === "") { // en caso de que no se ingrese un nombre
+        name = false;
+        document.getElementById("noneName").innerHTML = "Ingrese su nombre";
+    }
+    if (card === "") { // en caso de que no se ingrese un número de tarjeta
+        validationResult = false;
+        document.getElementById("invalidCard").innerHTML = "Ingrese un número de tarjeta"; // *No funciona este mensaje
+    }
+    if (validationResult && name) { // en caso de que se ingrese nombre y número de tarjeta válida
+        overlay.classList.add("active"); // Activa el modal
+        document.getElementById("addUser").innerHTML = "¡Felicidades " + name + "!"; // Titulo modal
+        document.getElementById("maskNumber").innerHTML = "Tu suscripción ha sido realizado con éxito.<br>" +
+            "Su número de tarjeta " + maskNumber + " es válida.<br>" +
+            "Ahora podrás disfrutar de ver material inédito de tus artistas favoritos.<br>" + "Gracias por suscribirte <3" // texto tarjeta válida 
+    }
+    if (validationResult!==true) { // en caso de que el número de tarjeta es
+        document.getElementById("invalidCard").innerHTML = "Su número de tarjeta " + maskNumber + " es inválida";
+    }
+})
+
+cardNumber.addEventListener('keyup', (x)  => {
+	let onlyNumber = x.target.value;
+  cardNumber.value = onlyNumber.replace(/\s/g, '').replace(/\D/g, '');
+})
+
+/*
+cardNumber.addEventListener("keyup", function hiddenNumber() {
+    let cardNumberHidd = cardNumber.value;
+    let maskifyResult = validator.maskify(cardNumberHidd);
+
+    cardNumber.value = maskifyResult;
+})
+*/
+//numero tarjeta válido 4289746659335873
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,50 @@
 const validator = {
-  // ...
-};
+
+  isValid: function (card) { // función de validación, se toma card como entrada (card = arrayMaskify.join("")) 
+
+      const arrayCardNumber = card.split("").reverse().map(Number); //convertir nº tarjeta en array, obtener reversa y convertir en number
+      let sum = 0 // variable para sumar dígitos
+      
+
+      for (let i = 0; i < arrayCardNumber.length; i++) {
+          if (i % 2 === 1) {
+              let evenNumber = arrayCardNumber[i] * 2;
+              if (evenNumber > 9) {
+                  evenNumber = 1 + evenNumber % 10;
+              }
+              sum += evenNumber;
+          } else {
+              sum += arrayCardNumber[i];
+          }
+      }
+      let result = sum % 10 // variable para obtener el residuo de sum módulo 10
+      if (result === 0) {
+          return true
+      } else {
+
+          return false
+      }
+  },
+
+  maskify: function (card) {
+
+      let arrayCardNumber = card.split(""),
+          //arrayUnmasked = [], // Array vacío que irá guardando los números de cardNumber
+          maskifySum = ""; // sumar # para reemplazar números
+
+      for (let i = 0; i < arrayCardNumber.length; i++) {
+          if (i < arrayCardNumber.length - 4) {
+              //arrayUnmasked.push(arrayCardNumber[i]);
+              maskifySum += "#";
+          } else {
+              //arrayUnmasked.push(arrayCardNumber[i]);
+              maskifySum += arrayCardNumber[i];
+          }
+      }
+      return maskifySum
+  }
+}
+
+
 
 export default validator;
